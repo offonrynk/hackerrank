a2f07707e9847f491c41dffd124fdce5e3eb0eb1
offonrynk <nkrumahoffonry@gmail.com>
2020-01-03 16:29:36 +0100

    adding 30_days of code as a submodule

--- 30_days_of_code/day2/day2.py
+++
@@ -1,9 +0,0 @@
-mealCost = float(input())
-tipPercent = int(input())
-taxPercent = int(input())
-
-tip = (mealCost * tipPercent) / 100
-tax = (mealCost * taxPercent) / 100
-
-totalCost = mealCost + tip + tax
-print(round(totalCost))
